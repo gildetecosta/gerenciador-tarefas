235c994af8ae8f62a9105a82e81da322eca917c3
gildetecosta <gildetefirmino@gmail.com>
2022-07-10 13:42:15 -0300

    Integração Continua

--- gerenciador_tarefas/gerenciador.py
+++ gerenciador_tarefas/gerenciador.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI # importando a fastApi
+from fastapi import FastAPI  # importando a fastApi
 
-app = FastAPI() # criando a aplicação do zero
+app = FastAPI()  # criando a aplicação do zero
+
 TAREFAS = []
-# "@" é uma forma de vincular esta função listar com o verbo get no recurso de tarefas
+
+# @ é uma forma de vincular esta função listar com o verbo get no recurso de tarefas
 @app.get("/tarefas")
-def listar(): 
-        return TAREFAS
+def listar():
+    return TAREFAS
--- tests/test_gerenciador.py
+++ tests/test_gerenciador.py
@@ -1,25 +1,37 @@
-# Estes testes tem o objetivo que a API volte o codigo 200, retorna uma lista em json
-# e que esta lista tem um conteudo
-from fastapi.testclient import TestClient
+# Estes testes tem o objetivo que a API volte o codigo 200,retorna uma lista em json e que esta lista tem um conteudo
 from fastapi import status
-from gerenciador_tarefas.gerenciador import app, TAREFAS
+from fastapi.testclient import TestClient
+
+from gerenciador_tarefas.gerenciador import TAREFAS, app
+
 
 # definindo uma função de testes
-def test_quando_listar_tarefas_devo_ter_como_retorno_codigo_de_status_200( ):
-    cliente = TestClient(app) # criando um cliente de testes
-    resposta = cliente.get("/tarefas")# o verbo get traz a lista de tarefas que estou solicitando
-    assert resposta.status_code == status.HTTP_200_OK # checar se a expresão == é igual e traz resposta
+def test_quando_listar_tarefas_devo_ter_como_retorno_codigo_de_status_200():
+    # criando um cliente de testes
+    cliente = TestClient(app)  
+    resposta = cliente.get(
+        "/tarefas"
+    )  # o verbo get traz a lista de tarefas que estou solicitando
+    assert (
+        resposta.status_code == status.HTTP_200_OK
+    )  # checar se a expresão == é igual e traz resposta
+
 
 def test_quando_listar_tarefas_formato_de_retorno_deve_ser_json():
     cliente = TestClient(app)
     resposta = cliente.get("/tarefas")
-    assert resposta.headers["Content-Type"] == "application/json" # a resposta do cabeçalho seja do tipo json
+    assert (
+        resposta.headers["Content-Type"] == "application/json"
+    )  # a resposta do cabeçalho seja do tipo json
+
 
 def test_quando_listar_tarefas_retorno_deve_ser_uma_lista():
     cliente = TestClient(app)
     resposta = cliente.get("/tarefas")
-    assert isinstance(resposta.json(), list) # funcção isinstance do python verifica se é uma lista,
-    #pega a resposta e transforma este valor em formato json em uma lista
+    assert isinstance(
+        resposta.json(), list
+    )  # funcção isinstance do python verifica se é uma lista, pega a resposta e transforma este valor em formato json em uma lista
+
 
 def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_id():
     TAREFAS.append(
@@ -32,8 +44,7 @@ def test_quando_listar_tarefas_a_tarefa_retornada_deve_possuir_id():
     )
     cliente = TestClient(app)
     resposta = cliente.get("/tarefas")
-    assert "id" in resposta.json().pop() # aqui eu tive como resposta uma lista de tarefas eestou 
-    #pegando o primeiro elemento da lista que esta no ID
+    assert (
+        "id" in resposta.json().pop()
+    )  # aqui eu tive como resposta uma lista de tarefas eestou pegando o primeiro elemento da lista que esta no ID
     TAREFAS.clear()
-
-   
